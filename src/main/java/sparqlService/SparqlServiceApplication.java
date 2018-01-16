fa48431b694a9cb05f8b1c09e18dd89626cb7b43
Christoph Krieger <krieger.christoph1@gmail.com>
2018-01-16 13:07:01 +0100

    iterate over query results and return as json

--- src/main/java/sparqlService/SparqlServiceApplication.java
+++ src/main/java/sparqlService/SparqlServiceApplication.java
@@ -2,11 +2,11 @@ package sparqlService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @ComponentScan(basePackages = "sparqlService")
 @EnableAutoConfiguration
 public class SparqlServiceApplication {
@@ -15,12 +15,4 @@ public class SparqlServiceApplication {
 		SpringApplication.run(SparqlServiceApplication.class, args);
 	}
 
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.any())
-//				.paths(PathSelectors.any())
-//				.build();
-//	}
 }
--- src/main/java/sparqlService/web/SparqlQueryController.java
+++ src/main/java/sparqlService/web/SparqlQueryController.java
@@ -4,10 +4,14 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,25 +20,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import sparqlService.models.SolutionDTO;
+
 @RestController
 @RequestMapping(value = "/api", produces = "application/json")
 public class SparqlQueryController {
 
 	@PostMapping(value="/executeQuery")
-	public ResponseEntity<?> executeSparqlQuery(@RequestBody final String queryIn) {
+	public ResponseEntity<?> executeSparqlQuery() {
+		String queryIn = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+				"SELECT ?name ?knows\n" +
+				"WHERE {\n" +
+				"  SERVICE <https://patternpedia.github.io/rdf-playground/rdfa.html> {\n" +
+				"    ?s foaf:name ?name ." +
+				"    ?s foaf:knows ?knows .  }\n" +
+				"}";
 		Model model = ModelFactory.createDefaultModel();
 		Query query = QueryFactory.create(queryIn);
 
 		QueryExecution qe = QueryExecutionFactory.create(query, model);
-		ResultSet results = qe.execSelect();
+		Iterator<QuerySolution> results = qe.execSelect();
+		Set<Map<String, String>> result = new HashSet<Map<String, String>>();
+		for ( ; results.hasNext() ; )
+		{
+			QuerySolution soln = results.next();
+			Map<String, String> resultVariables = new HashMap<String, String>();
+			Iterator<String> varNames = soln.varNames();
 
-		ResultSetFormatter.out(System.out, results, query);
-		String resultAsText = ResultSetFormatter.asText(results);
+			for ( ; varNames.hasNext() ; ) {
+				String varName = varNames.next();
+				resultVariables.put(varName, soln.get(varName).toString());
+				System.out.print("varName: " + varName + " resultVariable: " + soln.get(varName).toString());
+			}
+			result.add(resultVariables);
+		}
 
-		System.out.print(resultAsText);
+		System.out.print(result.size());
 		// Free up resources used running the query
 		qe.close();
 
-		return new ResponseEntity<String>(resultAsText, HttpStatus.OK);
+		return new ResponseEntity<Set<Map<String, String>>>(result, HttpStatus.OK);
 	}
 }
