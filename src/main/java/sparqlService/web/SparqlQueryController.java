5aa5d80feace903653f2e53a307fb068192359ea
Christoph Krieger <krieger.christoph1@gmail.com>
2018-01-16 16:05:08 +0100

    add CORS

--- src/main/java/sparqlService/web/SparqlQueryController.java
+++ src/main/java/sparqlService/web/SparqlQueryController.java
@@ -5,13 +5,9 @@ import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
-import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.Resource;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,46 +19,44 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
-import sparqlService.models.SolutionDTO;
 
 @RestController
 @RequestMapping(value = "/api", produces = "application/json")
 public class SparqlQueryController {
 
 	@PostMapping(value="/executeQuery")
-	public ResponseEntity<?> executeSparqlQuery() {
-		String queryIn = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
-				"SELECT ?name ?knows\n" +
-				"WHERE {\n" +
-				"  SERVICE <https://patternpedia.github.io/rdf-playground/rdfa.html> {\n" +
-				"    ?s foaf:name ?name ." +
-				"    ?s foaf:knows ?knows .  }\n" +
-				"}";
+	public ResponseEntity<?> executeSparqlQuery(@RequestBody String queryIn) {
+//		String queryIn = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+//				"SELECT ?name ?knows\n" +
+//				"WHERE {\n" +
+//				"  SERVICE <https://patternpedia.github.io/rdf-playground/rdfa.html> {\n" +
+//				"    ?s foaf:name ?name ." +
+//				"    ?s foaf:knows ?knows .  }\n" +
+//				"}";
 		Model model = ModelFactory.createDefaultModel();
 		Query query = QueryFactory.create(queryIn);
 
 		QueryExecution qe = QueryExecutionFactory.create(query, model);
-		Iterator<QuerySolution> results = qe.execSelect();
+		Iterator<QuerySolution> querySolutions = qe.execSelect();
+
 		Set<Map<String, String>> result = new HashSet<Map<String, String>>();
-		for ( ; results.hasNext() ; )
+		for ( ; querySolutions.hasNext() ; )
 		{
-			QuerySolution soln = results.next();
-			Map<String, String> resultVariables = new HashMap<String, String>();
+			Map<String, String> resultVariablesOfSolution = new HashMap<String, String>();
+			QuerySolution soln = querySolutions.next();
 			Iterator<String> varNames = soln.varNames();
 
 			for ( ; varNames.hasNext() ; ) {
 				String varName = varNames.next();
-				resultVariables.put(varName, soln.get(varName).toString());
+				resultVariablesOfSolution.put(varName, soln.get(varName).toString());
 				System.out.print("varName: " + varName + " resultVariable: " + soln.get(varName).toString());
 			}
-			result.add(resultVariables);
+			result.add(resultVariablesOfSolution);
 		}
 
-		System.out.print(result.size());
 		// Free up resources used running the query
 		qe.close();
 
